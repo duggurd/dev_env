ffbb8cdb3f5b1a33d5da773b3c5731ef0eb2e8bc
duggurd <alexanderhaugerud@gmail.com>
2024-05-06 20:34:55 +0200

    update

--- setup.ps1
+++ setup.ps1
@@ -12,17 +12,17 @@ Write-Output "Installing VSCOde extension"
 ./vscode/extensions/vscode_extensions_install.ps1
 
 
-Write-Output "Installing PWSH Modules"
+# Write-Output "Installing PWSH Modules"
 
-# https://github.com/dahlbyk/posh-git
-Install-Module posh-git -Scope CurrentUser -Force
-Write-Output "Import-Module posh-git" >> "$HOME\Documents\PowerShell\Profile.ps1"
+# # https://github.com/dahlbyk/posh-git
+# Install-Module posh-git -Scope CurrentUser -Force
+# Write-Output "Import-Module posh-git" >> "$HOME\Documents\PowerShell\Profile.ps1"
 
-# https://github.com/junegunn/fzf#installation
-winget install fzf
+# # https://github.com/junegunn/fzf#installation
+# winget install fzf
 
-# https://github.com/kelleyma49/PSFzf
-Install-Module -Name PSFzf -Scope CurrentUser -Force
-Write-Output "Import-Module PSFzf" >> "$HOME\Documents\PowerShell\Profile.ps1"
+# # https://github.com/kelleyma49/PSFzf
+# Install-Module -Name PSFzf -Scope CurrentUser -Force
+# Write-Output "Import-Module PSFzf" >> "$HOME\Documents\PowerShell\Profile.ps1"
 
-Write-Output "Reboot required to finish setup."
+# Write-Output "Reboot required to finish setup."
--- vscode/extensions/vscode_extensions_install.ps1
+++ vscode/extensions/vscode_extensions_install.ps1
@@ -1,6 +1,5 @@
 $extensions = (
     "DavidAnson.vscode-markdownlint",
-    "ms-azuretools.vscode-docker",
     "ms-python.isort",
     "ms-python.python",
     "ms-python.vscode-pylance",
@@ -9,14 +8,20 @@ $extensions = (
     "ms-toolsai.jupyter-renderers",
     "ms-toolsai.vscode-jupyter-cell-tags",
     "ms-toolsai.vscode-jupyter-slideshow",
-    "ms-vscode-remote.remote-containers",
-    "unifiedjs.vscode-mdx",
-    "ms-vscode.powershell",
-    "mechatroner.rainbow-csv",
-    "1YiB.rust-bundle",
+    "1yib.rust-bundle",
     "bradlc.vscode-tailwindcss",
-    "ms-vscode-remote.remote-wsl",
-    "ms-azuretools.vscode-docker"
+    "dustypomerleau.rust-syntax",
+    "esbenp.prettier-vscode",
+    "github.vscode-github-actions",
+    "golang.go",
+    "hashicorp.terraform",
+    "mechatroner.rainbow-csv",
+    "ms-kubernetes-tools.vscode-kubernetes-tools",
+    "redhat.vscode-yaml",
+    "rust-lang.rust-analyzer",
+    "serayuzgur.crates",
+    "unifiedjs.vscode-mdx",
+    "yzhang.markdown-all-in-one"
 )
 
 foreach ($item in $extensions) {
